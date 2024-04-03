b6993d8d349096b28fb9ddee0b71c802111f6b1e
Aybüke Betül Yarım <aybukebetulyarim@gmail.com>
2024-04-03 08:34:38 +0300

    initial commit

---
+++ src/test/java/com/reactive/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
